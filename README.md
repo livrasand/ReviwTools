b3d5043822857aa300a81384400385373fb96e7c
Livrädo Sandoval <104039397+livrasand@users.noreply.github.com>
2023-08-27 14:39:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# ReviwTools para Sublime Text
+
+Este es un complemento para Sublime Text que permite ordenar los bloques JSON por nombre y mostrar sus posiciones en <a href="https://livrasand.github.io/jwpub-catalog.html">JWPUB Catalog</a>.
+
+La funcionalidad incluye:
+
+- Probar un archivo JSON del repositorio local
+- Probar una URL JSON de repositorio remoto
+- Actualización de un archivo JSON del repositorio local
+
+## Instalación
+
+La instalación se realiza a través de <a href="https://packagecontrol.io/">Package Control</a>.
+
+## Uso
+
+1. Abre el archivo JSON que contiene bloques de datos de los archivos JWPUB.
+2. Ejecuta el comando `"ReviwTools: Test Default File"` desde la paleta de comandos.
+3. Los bloques se ordenarán por nombre y se mostrarán sus posiciones en un panel lateral.
+
+## Contacto
+
+Si tienes preguntas, comentarios o sugerencias, no dudes en [contactarme](mailto:livrasand@outlook.com).
+
+---
+
+**Nota:** Este complemento fue desarrollado por [Livrädo Sandoval](https://github.com/livrasand). Si encuentras algún problema o tienes ideas de mejora, ¡estaré encantado de escucharte!
---
+++ ReviwTools.py
@@ -0,0 +1,41 @@
+import sublime
+import sublime_plugin
+import json
+
+class ReviwToolsTestDefaultFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        region = sublime.Region(0, self.view.size())
+        content = self.view.substr(region)
+
+        try:
+            data_list = json.loads(content)
+            sorted_data = sorted(data_list, key=lambda x: x['name'].lower())
+            sorted_content = json.dumps(sorted_data, indent=4, ensure_ascii=False)
+            self.view.replace(edit, region, sorted_content)
+
+            # Obtener el nombre de cada bloque y su posición
+            block_positions = [
+                "{} está en la posición {}".format(block['name'], i + 1)
+                for i, block in enumerate(sorted_data)
+            ]
+            
+            # Mostrar mensajes de posición en un panel lateral personalizado
+            self.show_custom_panel("\n".join(block_positions))
+        except Exception as e:
+            error_message = "Error sorting JSON: " + str(e)
+            sublime.error_message(error_message)
+            self.show_copy_button(error_message)
+
+    def show_custom_panel(self, message):
+        output_view = self.view.window().create_output_panel("review_tools_custom_panel")
+        output_view.run_command("insert", {"characters": message})
+        output_view.set_read_only(True)
+        self.view.window().run_command("show_panel", {"panel": "output.review_tools_custom_panel"})
+
+    def show_copy_button(self, message):
+        def copy_to_clipboard(text):
+            sublime.set_clipboard(text)
+
+        sublime.active_window().show_input_panel(
+            "Error Message:", message, copy_to_clipboard, None, None
+        )
